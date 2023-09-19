772be0a51684b89b20f5e423c86146c29de2d65e
hanmingrui159 <36434002+hanmingrui159@users.noreply.github.com>
2023-09-19 02:23:23 -0400

    test

---
+++ main.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Category(Enum):
+    TOOLS = 'tools'
+    CONSUMABLES = 'consumables'
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    count: int
+    id: int
+    category: Category
+
+
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
+    1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
+    2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
+}
+
+
+# FastAPI handles JSON serialization and deserialization for us.
+# We can simply use built-in python and Pydantic types, in this case dict[int, Item].
+@app.get("/")
+def index() -> dict[str, dict[int, Item]]:
+    return {"items": items}
+
+@app.get("/items/{item_id}")
+def query_item_by_id(item_id: int) -> Item:
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404, detail=f"Item with {item_id=} does not exist."
+        )
+    return items[item_id]
+
+@app.post("/")
+def add_item(item: Item) -> dict[str, Item]:
+
+    if item.id in items:
+        HTTPException(status_code=400, detail=f"Item with {item.id=} already exists.")
+
+    items[item.id] = item
+    return {"added": item}
